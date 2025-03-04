b0557103d2c1135956f614ab5554c9804154ebb1
Mica CF <mcohenfuentes@gmail.com>
2025-03-04 11:25:52 -0500

    exercise 5

--- app.js
+++ app.js
@@ -54,3 +54,66 @@ const car = {
 const { make, model }  = car;
 console.log(make)
 console.log(model)
+
+// ! Exercise 4: 
+// a. Duplicate the provided `morePizzaToppings` array using the spread 
+//    operator and assign it to a variable named `uncontroversialPizzaToppings`.
+// 
+// b. Console log the `uncontroversialPizzaToppings` variable.
+//
+// Starting code (don't modify this):
+
+const morePizzaToppings = ['Cheese', 'Sauce'];
+
+
+// Your code here:
+
+const uncontroversialPizzaToppings = [...morePizzaToppings]
+console.log(uncontroversialPizzaToppings)
+
+
+// ! Exercise 5:
+// a. Duplicate the provided `anotherCar` object and spread its values into a 
+//    new variable named `myCar`.
+//
+// b. Change the `make` and `model` properties of the `myCar` object to new 
+//    values.
+//
+// c. Console log both objects and observe the results.
+//
+// Starting code (don't modify this):
+
+const anotherCar = {
+    make: 'Toyota',
+    model: 'RAV4',
+  };
+  
+  // Your code here:
+  
+  const myCar = {...anotherCar}
+  myCar.make = 'Nissan';
+  myCar.model = 'Frontier'
+  
+  console.log('Original: ', anotherCar)
+  console.log('Clone: ', myCar)
+
+  // ! Exercise 6:
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+const propertyName = 'username'
+
+const userProfile = {
+    [propertyName]: 'Shmica'
+}
+
+console.log(userProfile)
